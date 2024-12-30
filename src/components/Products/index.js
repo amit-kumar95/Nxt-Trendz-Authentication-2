99a38ba5bff31c4f7fba6c43801e779506adeff1
amit-kumar95 <amitratan15012000@gmail.com>
2024-12-30 06:26:31 +0000

    commit message

---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="productCont">
+      <img
+        className="productImgStl"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png "
+        alt="products"
+      />
+    </div>
+  </>
+)
+
+export default Products
